37d9073367b276c9f047c85a58833145ea72db89
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2024-09-18 09:58:07 -0400

    Removing comments

--- ether-coin-flip/src/App.js
+++ ether-coin-flip/src/App.js
@@ -7,7 +7,7 @@ const contractAddress = "0x12Eb0E4591fD62B3e8af390C81e5E111b1CE5003";
 const abi = ABI;
 
 // Using Base Sepolia
-const baseSepoliaChainId = "84532"; // Correct Chain ID in hexadecimal for 84532
+const baseSepoliaChainId = "84532";
 
 const baseSepoliaParams = {
   chainId: baseSepoliaChainId,
@@ -17,8 +17,8 @@ const baseSepoliaParams = {
     symbol: "ETH",
     decimals: 18,
   },
-  rpcUrls: ["https://sepolia.base.org"], // Replace with a working RPC provider
-  blockExplorerUrls: ["https://base-sepolia.blockscout.com/"], // Sepolia block explorer
+  rpcUrls: ["https://sepolia.base.org"],
+  blockExplorerUrls: ["https://base-sepolia.blockscout.com/"],
 };
 
 let provider, signer, contract;
